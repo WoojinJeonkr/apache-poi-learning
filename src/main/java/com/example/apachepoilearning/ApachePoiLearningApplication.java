1f281b980d467de48b493872dbe15aaa0754abff
WoojinJeonkr <wooeric1@naver.com>
2025-08-08 22:04:18 +0900

    init project

---
+++ src/main/java/com/example/apachepoilearning/ApachePoiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.apachepoilearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePoiLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachePoiLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apachepoilearning/ApachePoiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachepoilearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachePoiLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
